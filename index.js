f3c0e7252e2e78cbee22d1ecd921c5fc14371141
AmosAidoo <aidooamos20@gmail.com>
2019-07-30 13:36:34 +0000

    Entry Point

---
+++ index.js
@@ -0,0 +1,7 @@
+const compilersClient = require("./src/api/compilers-client-v4");
+const problemsClient = require("./src/api/problems-client-v4");
+
+module.exports = {
+	compilersClientV4: compilersClient,
+	problemsClientV4: problemsClient
+}
